c93d4b9c05f64ee3cad6cfe6f50b654d55c0b638
Unmeinh <khongphaihoanglinh@gmail.com>
2023-12-19 00:13:23 +0700

    Fix token device when registing

--- src/views/form/ConfirmedShop.js
+++ src/views/form/ConfirmedShop.js
@@ -7,8 +7,6 @@ import React, { useState } from 'react'
 import styles from '../../styles/all.style';
 import { storageMMKV } from '../../storage/storageMMKV';
 import { useNavigation } from '@react-navigation/native';
-import messaging from '@react-native-firebase/messaging';
-import database from '@react-native-firebase/database';
 
 export default function ConfirmedShop() {
     const navigation = useNavigation();
@@ -18,67 +16,6 @@ export default function ConfirmedShop() {
         storageMMKV.setValue('login.isLogin', false);
         navigation.replace('LoginScreen');
     }
-
-    async function getToken() {
-        let token = await messaging().getToken();
-        if (token) {
-            await sendTokenToFirebase(token);
-        } else {
-            Toast.show({
-                type: 'error',
-                position: 'top',
-                text1: 'Có lỗi xảy ra trong lúc gửi yêu cầu!\nVui lòng thử lại sau!'
-            });
-        }
-    }
-
-    const sendTokenToFirebase = async (newToken) => {
-        try {
-            const databaseRef = database().ref('/sellerTokens');
-            const tokenData = {
-                token: newToken,
-            };
-            await databaseRef.push(tokenData);
-            storageMMKV.setValue('hasSentToken', true);
-            storageMMKV.setValue('tokenDevice', newToken);
-            if (storageMMKV.getString('login.token')) {
-                let res = await onAxiosPost('shop/autoLogin', {
-                    tokenDevice: newToken
-                }, 'json');
-                if (res) {
-                    if (res?.data.isApproved == 1) {
-                        navigation.replace('NaviTabScreen');
-                    }
-                    if (res?.data.isApproved == -1) {
-                        navigation.replace('DeniedShop');
-                    }
-                } else {
-                    storageMMKV.setValue('login.token', "");
-                }
-            }
-        } catch (error) {
-            console.error('Lỗi khi gửi token đến Firebase:', error);
-        }
-    };
-
-    React.useEffect(() => {
-        const unsub = navigation.addListener('focus', () => {
-            const hasSentToken = storageMMKV.getBoolean('hasSentToken');
-            const tokenDevice = storageMMKV.getString('tokenDevice');
-            if (!hasSentToken) {
-                getToken();
-            } else {
-                if (!tokenDevice) {
-                    getToken();
-                }
-            }
-            return () => {
-                unsub.remove();
-            };
-        });
-
-        return unsub;
-    }, [navigation]);
 
     return (
         <View style={{ backgroundColor: '#FEF6E4', flex: 1 }}>
